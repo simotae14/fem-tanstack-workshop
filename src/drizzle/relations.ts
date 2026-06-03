17a71a7625daf3f0bf64ae06af61ce79d0830160
Simone Taeggi <startae14@gmail.com>
2026-06-03 17:45:09 +0200

    01 Quick Drizzle Demo

--- src/drizzle/relations.ts
+++ src/drizzle/relations.ts
@@ -1,69 +1,62 @@
 import { defineRelations } from "drizzle-orm";
 import * as schema from "./schema";
 
-export const relations = defineRelations(schema, r => ({
-  bodyCompositionMeasurement: {
-    bodyCompositionMetric: r.one.bodyCompositionMetric({
-      from: r.bodyCompositionMeasurement.bodyCompositionMetricId,
-      to: r.bodyCompositionMetric.id,
-    }),
-  },
-  bodyCompositionMetric: {
-    bodyCompositionMeasurements: r.many.bodyCompositionMeasurement(),
-  },
-  workoutSegment: {
-    workout: r.one.workout({
-      from: r.workoutSegment.workoutId,
-      to: r.workout.id,
-    }),
-    exercises: r.many.exercises(),
-  },
-  workout: {
-    workoutSegments: r.many.workoutSegment(),
-  },
-  exercises: {
-    workoutSegments: r.many.workoutSegment({
-      from: r.exercises.id.through(r.workoutSegmentExercise.exerciseId),
-      to: r.workoutSegment.id.through(
-        r.workoutSegmentExercise.workoutSegmentId,
-      ),
-    }),
-    workoutTemplateSegments: r.many.workoutTemplateSegment(),
-  },
-  workoutSegmentExerciseMeasurement: {
-    workoutSegmentExercise: r.one.workoutSegmentExercise({
-      from: r.workoutSegmentExerciseMeasurement.workoutSegmentExerciseId,
-      to: r.workoutSegmentExercise.id,
-    }),
-  },
-  workoutSegmentExercise: {
-    workoutSegmentExerciseMeasurements:
-      r.many.workoutSegmentExerciseMeasurement(),
-  },
-  workoutTemplateSegment: {
-    workoutTemplate: r.one.workoutTemplate({
-      from: r.workoutTemplateSegment.workoutTemplateId,
-      to: r.workoutTemplate.id,
-    }),
-    exercises: r.many.exercises({
-      from: r.workoutTemplateSegment.id.through(
-        r.workoutTemplateSegmentExercise.workoutTemplateSegmentId,
-      ),
-      to: r.exercises.id.through(r.workoutTemplateSegmentExercise.exerciseId),
-    }),
-  },
-  workoutTemplate: {
-    workoutTemplateSegments: r.many.workoutTemplateSegment(),
-  },
-  workoutTemplateSegmentExerciseMeasurement: {
-    workoutTemplateSegmentExercise: r.one.workoutTemplateSegmentExercise({
-      from: r.workoutTemplateSegmentExerciseMeasurement
-        .workoutTemplateSegmentExerciseId,
-      to: r.workoutTemplateSegmentExercise.id,
-    }),
-  },
-  workoutTemplateSegmentExercise: {
-    workoutTemplateSegmentExerciseMeasurements:
-      r.many.workoutTemplateSegmentExerciseMeasurement(),
-  },
-}));
+export const relations = defineRelations(schema, (r) => ({
+	bodyCompositionMeasurement: {
+		bodyCompositionMetric: r.one.bodyCompositionMetric({
+			from: r.bodyCompositionMeasurement.bodyCompositionMetricId,
+			to: r.bodyCompositionMetric.id
+		}),
+	},
+	bodyCompositionMetric: {
+		bodyCompositionMeasurements: r.many.bodyCompositionMeasurement(),
+	},
+	workoutSegment: {
+		workout: r.one.workout({
+			from: r.workoutSegment.workoutId,
+			to: r.workout.id
+		}),
+		exercises: r.many.exercises(),
+	},
+	workout: {
+		workoutSegments: r.many.workoutSegment(),
+	},
+	exercises: {
+		workoutSegments: r.many.workoutSegment({
+			from: r.exercises.id.through(r.workoutSegmentExercise.exerciseId),
+			to: r.workoutSegment.id.through(r.workoutSegmentExercise.workoutSegmentId)
+		}),
+		workoutTemplateSegments: r.many.workoutTemplateSegment(),
+	},
+	workoutSegmentExerciseMeasurement: {
+		workoutSegmentExercise: r.one.workoutSegmentExercise({
+			from: r.workoutSegmentExerciseMeasurement.workoutSegmentExerciseId,
+			to: r.workoutSegmentExercise.id
+		}),
+	},
+	workoutSegmentExercise: {
+		workoutSegmentExerciseMeasurements: r.many.workoutSegmentExerciseMeasurement(),
+	},
+	workoutTemplateSegment: {
+		workoutTemplate: r.one.workoutTemplate({
+			from: r.workoutTemplateSegment.workoutTemplateId,
+			to: r.workoutTemplate.id
+		}),
+		exercises: r.many.exercises({
+			from: r.workoutTemplateSegment.id.through(r.workoutTemplateSegmentExercise.workoutTemplateSegmentId),
+			to: r.exercises.id.through(r.workoutTemplateSegmentExercise.exerciseId)
+		}),
+	},
+	workoutTemplate: {
+		workoutTemplateSegments: r.many.workoutTemplateSegment(),
+	},
+	workoutTemplateSegmentExerciseMeasurement: {
+		workoutTemplateSegmentExercise: r.one.workoutTemplateSegmentExercise({
+			from: r.workoutTemplateSegmentExerciseMeasurement.workoutTemplateSegmentExerciseId,
+			to: r.workoutTemplateSegmentExercise.id
+		}),
+	},
+	workoutTemplateSegmentExercise: {
+		workoutTemplateSegmentExerciseMeasurements: r.many.workoutTemplateSegmentExerciseMeasurement(),
+	},
+}))
